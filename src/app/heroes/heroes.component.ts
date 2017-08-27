99f91de190eac7e779d0893813f4cfebd7bb44e7
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-08-27 14:07:08 +0200

    -restructure -see excert, navigate to detail -stronger hidden selector

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from '../shared/Hero';
 import { HeroService } from '../shared/hero.service';
 
@@ -9,9 +11,10 @@ import { HeroService } from '../shared/hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) {
-
-  }
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) { }
 
   hero: Hero = {
     id: null,
@@ -29,5 +32,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(){
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
