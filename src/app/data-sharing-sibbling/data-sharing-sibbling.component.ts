614cc0eb074c94274dcab5dbe286c0b0b2ccfe65
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-09-10 13:39:32 +0200

    -working on data sharing between components

--- src/app/data-sharing-sibbling/data-sharing-sibbling.component.ts
+++ src/app/data-sharing-sibbling/data-sharing-sibbling.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataSharingMediatorService } from 'app/shared/data-sharing-mediator.service';
 
 @Component({
     selector: 'my-data-sharing-sibbling',
     templateUrl: './data-sharing-sibbling.component.html'
 })
-export class DataSharingSibblingComponent { 
+export class DataSharingSibblingComponent implements OnInit {
+
+    message: string;
+
+    constructor(
+        private dataSharingMediatorService: DataSharingMediatorService
+    ) {}
+
+    ngOnInit() {
+        this.dataSharingMediatorService.currentMessage.subscribe(message => this.message = message);
+    }
 
 }
--- src/app/data-sharing/data-sharing.component.ts
+++ src/app/data-sharing/data-sharing.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataSharingMediatorService } from 'app/shared/data-sharing-mediator.service';
 
 @Component({
     selector: 'my-data-sharing',
     templateUrl: './data-sharing.component.html'
 })
-export class DataSharingComponent { 
+export class DataSharingComponent implements OnInit {
+    message: string;
 
+    constructor(
+        private dataSharingMediatorService: DataSharingMediatorService
+    ) { }
+
+    ngOnInit() {
+        this.dataSharingMediatorService.currentMessage.subscribe(message => this.message = message);
+    }
 }
