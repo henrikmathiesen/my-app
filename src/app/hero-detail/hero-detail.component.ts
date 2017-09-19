97ee201ffbfe8d1e6bc394924735730de5ee65b7
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-09-19 20:41:30 +0200

    -testing bindings when navigating to same component but different id

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
@@ -10,7 +10,7 @@ import { HeroService } from '../shared/hero.service';
     selector: 'my-hero-detail',
     templateUrl: './hero-detail.component.html'
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent implements OnInit, OnDestroy {
     constructor(
         private heroService: HeroService,
         private route: ActivatedRoute,
@@ -18,15 +18,22 @@ export class HeroDetailComponent implements OnInit {
     ) { }
 
     hero: Hero;
+    testRouteChangeBindings: string;
 
     ngOnInit() {
+        console.log('HeroDetailComponent ngOnInit');
+
         // https://stackoverflow.com/questions/42655134/angular-2-why-use-switchmap-when-retrieving-route-params
         this.route.paramMap
-            .switchMap((params: ParamMap) =>  this.heroService.getHero(+params.get('id')))
+            .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
             .subscribe((hero: Hero) => this.hero = hero);
     }
 
-    goBack(){
+    ngOnDestroy() {
+        console.log('HeroDetailComponent ngOnDestroy');
+    }
+
+    goBack() {
         this.location.back();
     }
 }
