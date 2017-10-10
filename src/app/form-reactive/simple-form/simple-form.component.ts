c23b8b2e6c2a42fec14d75161247f1a260434165
henmat <henrik.mathiesen@knowit.se>
2017-10-10 17:08:24 +0200

    -simple form

--- src/app/form-reactive/simple-form/simple-form.component.ts
+++ src/app/form-reactive/simple-form/simple-form.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';    // These are different that for Template Driven Forms
 
 @Component({
     selector: 'simple-form',
     templateUrl: './simple-form.component.html'
 })
 export class SimpleFormComponent {
-    
+    name: FormControl;
+
+    constructor(){
+        this.name = new FormControl();
+    }
+
+    onSubmit() {
+        console.log('submit', this.name);
+    }
 }
