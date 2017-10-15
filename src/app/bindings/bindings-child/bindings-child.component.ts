4082f42a5c5b51a625d6b684ee7abfc4d064e34c
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-10-15 08:42:31 +0200

    -ngOnChanges

--- src/app/bindings/bindings-child/bindings-child.component.ts
+++ src/app/bindings/bindings-child/bindings-child.component.ts
@@ -1,18 +1,22 @@
-import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 import { random } from 'lodash';
 import { IJsonPlaceholder } from '../IJsonPlaceholder';
 import { JsonPlaceholderService } from '../jsonPlaceholder.service';
+
+interface ISimpleChanges extends SimpleChanges {
+    jsonData: SimpleChange;
+}
 
 @Component({
     selector: 'bindings-child',
     templateUrl: './bindings-child.component.html'
 })
-export class BindingsChildComponent implements OnInit, OnDestroy {
+export class BindingsChildComponent implements OnInit, OnDestroy, OnChanges {
 
     @Input() jsonData: IJsonPlaceholder; // = { id: 0, body: 'default', title: 'default', userId: 0 }; Can set default value on @Input() , can also map to another [name] by @Input('name')
     @Output() change: EventEmitter<IJsonPlaceholder> = new EventEmitter<IJsonPlaceholder>(); // it will only emit an event to the immediate parent component.
 
-    testingViewChild:string;
+    testingViewChild: string;
 
     constructor(
         private jsonPlaceholderService: JsonPlaceholderService
@@ -25,6 +29,13 @@ export class BindingsChildComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.testingViewChild = 'hello from bindings child via @ViewChild';
         console.log('init: bindings-child component');
+    }
+
+    ngOnChanges(changes: ISimpleChanges) {
+        // We can listen for changes in @Inputs an act on them like this
+        if (changes.jsonData) {
+            console.log('changes.jsonData', changes.jsonData);
+        }
     }
 
     ngOnDestroy() {
