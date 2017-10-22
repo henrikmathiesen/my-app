7d434e98365b4d7d198e265dfcb0f12da161a9a3
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-10-22 19:49:49 +0200

    -comments

--- src/app/form-reactive/little-more-form/little-more-form.component.ts
+++ src/app/form-reactive/little-more-form/little-more-form.component.ts
@@ -57,7 +57,9 @@ export class LittleMoreFormComponent implements OnInit, IModel {
     this.validate = post.validate;
   }
 
-  showValidationError(name: string) {
-    return !this.littleMoreForm.controls[name].valid && this.littleMoreForm.controls[name].touched;
+  showValidationError(formControlName: string) {
+    // Can use either of these
+    //return !this.littleMoreForm.controls[name].valid && this.littleMoreForm.controls[name].touched;
+    return !this.littleMoreForm.get(formControlName).valid && this.littleMoreForm.get(formControlName).touched;
   }
 }
