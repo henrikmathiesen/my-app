282d2c5b09355d4ea711ed9cceb13c4a4894093c
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-11-11 08:25:15 +0100

    -removed unused api constants

--- src/app/shared/constants-api.service.ts
+++ src/app/shared/constants-api.service.ts
@@ -5,16 +5,4 @@ export class ConstantsApiService {
     getJsonPlaceHolderUrl() {
         return 'http://jsonplaceholder.typicode.com/posts/';
     }
-
-    getGamesUrl() {
-        return 'http://localhost:1337/api/'
-    }
-
-    getGamesEntities(){
-        return {
-            users: 'users',
-            games: 'games',
-            reviews: 'reviews'
-        }
-    }
 }
