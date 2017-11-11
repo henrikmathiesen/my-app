cf1fe827b36b435c0ed511510a47de581565a583
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-11-11 08:32:31 +0100

    -changed to https because needed

--- src/app/shared/constants-api.service.ts
+++ src/app/shared/constants-api.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConstantsApiService {
     getJsonPlaceHolderUrl() {
-        return 'http://jsonplaceholder.typicode.com/posts/';
+        return 'https://jsonplaceholder.typicode.com/posts/';
     }
 }
