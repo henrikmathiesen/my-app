ea4056311f9c85a1f338cce937afcb6c3db9b0ec
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-11-19 10:21:45 +0100

    -creating another module with a service

---
+++ src/app/another-module/another.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AnotherService } from './services/another.service';
+
+@NgModule({
+    providers: [
+        AnotherService
+    ]
+})
+export class AnotherModule { }
