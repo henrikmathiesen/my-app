d777d02a6dd78f41c7f0ff46aa45b255e3943936
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-12-23 14:20:02 +0100

    -comments

--- src/app/bindings/bindings.component.ts
+++ src/app/bindings/bindings.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, OnDestroy, AfterViewInit, ElementRef } fr
 import random from 'lodash.random'
 import { IPost } from 'app/shared/models/post.interface';
 import { PostByIdService } from './post-by-id.service';
-import { BindingsChildComponent } from './bindings-child/bindings-child.component';                                      // 1) Can access sub component via ViewChild
+import { BindingsChildComponent } from './bindings-child/bindings-child.component';                              // 1) Can import sub component
 
 @Component({
     selector: 'my-bindings',
@@ -11,8 +11,8 @@ import { BindingsChildComponent } from './bindings-child/bindings-child.componen
 })
 export class BindingsComponent implements OnInit, OnDestroy, AfterViewInit {
 
-    @ViewChild(BindingsChildComponent) bindingsChild: BindingsChildComponent;                                   // 2)
-    @ViewChild('theSpan') theSpan: ElementRef;                                                                  // A
+    @ViewChild(BindingsChildComponent) bindingsChild: BindingsChildComponent;                                   // 2) And access it via ViewChild
+    @ViewChild('theSpan') theSpan: ElementRef;                                                                  // A) Can grab a DOM element like this
 
     post: IPost;
 
@@ -27,8 +27,7 @@ export class BindingsComponent implements OnInit, OnDestroy, AfterViewInit {
     ngOnInit() {
         console.log('init: bindings component');
 
-        // Can manipulate DOM elements like this. Allthough should we?
-        (this.theSpan.nativeElement as HTMLElement).classList.add('test');                                        // B
+        (this.theSpan.nativeElement as HTMLElement).classList.add('test');                                        // B) And manipulate it (Allthough should we?)
 
         this.postByIdService.get(this.getRandomInt()).then(data => {
             this.post = data
@@ -37,7 +36,7 @@ export class BindingsComponent implements OnInit, OnDestroy, AfterViewInit {
 
     ngAfterViewInit() {
         // Here we can access the data from the sub component via @ViewChild, but changes in subcomponent does not propogate upwards. Need @Output for that.
-        console.log('bindingsChild', this.bindingsChild.testingViewChild);                                      // 3
+        console.log('bindingsChild', this.bindingsChild.testingViewChild);                                          // 3
     }
 
     ngOnDestroy() {
@@ -50,7 +49,7 @@ export class BindingsComponent implements OnInit, OnDestroy, AfterViewInit {
         });
     }
 
-    change(event: IPost) {
+    change(event: IPost) {                                                                                            // 4) Child is outputting a change event that we can bind to (see template also)
         this.post = event;
     }
 }
