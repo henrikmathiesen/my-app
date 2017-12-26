097cb1c643768ee38ff8fdbc13b5fe913464ac70
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-12-26 16:05:31 +0100

    -Labbing with ExpressionChangedAfterItHasBeenChecked

--- src/app/ecaihbce/ecaihbce-child/ecaihbce-child.component.ts
+++ src/app/ecaihbce/ecaihbce-child/ecaihbce-child.component.ts
@@ -1,6 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
+import { EcaihbceComponent } from '../ecaihbce.component';
 
 @Component({
+    selector: 'my-ecaihbce-child',
     templateUrl: './ecaihbce-child.component.html'
 })
-export class EcaihbceChildComponent { }
+export class EcaihbceChildComponent implements OnInit, AfterViewInit {
+    @Input() text: string;
+
+    constructor(
+        private ecaihbceComponent: EcaihbceComponent
+    ) { }
+
+    ngOnInit() {
+        // 1) Here we change the text property of the parent
+        // after the bindings (onChanges) has been processed
+
+        // In other words we update the text property on the parent
+        // after it is sent to the child
+
+        // This results in the error: ExpressionChangedAfterItHasBeenChecked
+
+        // this.ecaihbceComponent.text = 'updated text';
+
+        // -----------------------------------------------------------------
+
+        // 2A) The name property is rendered in the DOM in the parent component.
+        // If we change it here BEFORE it is rendered in the DOM in parent
+        // then there is no error.
+        // this.ecaihbceComponent.name = 'updated name';
+    }
+
+    ngAfterViewInit() {
+        // 2B) However if we change it in this life cycle hook, AFTER it is rendered in DOM in parent
+        // then we get the ExpressionChangedAfterItHasBeenChecked error.
+        // this.ecaihbceComponent.name = 'updated name';
+    }
+}
--- src/app/ecaihbce/ecaihbce.component.ts
+++ src/app/ecaihbce/ecaihbce.component.ts
@@ -1,6 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
     templateUrl: './ecaihbce.component.html'
 })
-export class EcaihbceComponent {}
+export class EcaihbceComponent implements OnInit, AfterViewInit {
+    name = 'Iam parent component';
+    text = 'A message for the child component';
+
+    constructor(
+        // private cd: ChangeDetectorRef , can be used to manually detect changes: this.cd.detectChanges();
+    ) { }
+
+    ngOnInit() {
+        // Changing the text property, that is sent to the child, here is ok, no error.
+        // this.text = 'updated text, parent ngOnInit';
+    }
+
+    ngAfterViewInit() {
+        // However changing it here, AFTER it is sent to the child, results in 
+        // ExpressionChangedAfterItHasBeenChecked error
+        // this.text = 'updated text, parent ngAfterViewInit';
+    }
+}
