5da729160d951f819c1d4aa3258a127b4c8cb883
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-12-27 09:14:47 +0100

    -removed unused selector -text info

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-rxjs',
     templateUrl: './rxjs.component.html'
 })
-export class RxJsComponent { }
+export class RxJsComponent { 
+    
+}
