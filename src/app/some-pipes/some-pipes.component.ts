77e7e008760c83a063bc1024086e131df661290b
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-05-30 18:54:24 +0200

    -more dates with pipes

--- src/app/some-pipes/some-pipes.component.ts
+++ src/app/some-pipes/some-pipes.component.ts
@@ -11,6 +11,7 @@ export class SomePipesComponent implements OnInit {
 
   obj: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
   vacation = new Date('2018-07-01T06:00:00+00:00');
+  someDate = new Date(2019, 0, 10, 8, 0);
 
   ngOnInit() {
   }
