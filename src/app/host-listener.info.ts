480599640c8995d9c30ca1feda257d21a91ced9f
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-06-07 19:43:12 +0200

    -documentation, herding code

---
+++ src/app/host-listener.info.ts
@@ -0,0 +1,35 @@
+/*
+
+    // some.component.ts
+
+    import { Component, ElementRef, HostListener } from '@angular/core';
+
+    constructor(
+        private el: ElementRef
+    ) { }
+
+    @HostListener(UIEventsConstant.documentClick, ['$event'])
+    closeDatePickerIfClickWasOutside(event: Event) {
+        const nativeElement = this.el.nativeElement;
+        if (!nativeElement.contains(event.target)) {
+            this.d.close();
+        }
+    }
+
+    @HostListener(UIEventsConstant.documentKeyup, ['$event'])
+    closeDatePickerIfKeyWasEsc(event: any) {
+        if (event.key === UIEventsConstant.escape) {
+            this.d.close();
+        }
+    }
+    
+    
+    // ui-events.constant.ts
+    
+    export class UIEventsConstant {
+        static readonly documentClick = 'document:click';
+        static readonly documentKeyup = 'document:keyup';
+        static readonly escape = 'Escape';
+    }
+
+*/
