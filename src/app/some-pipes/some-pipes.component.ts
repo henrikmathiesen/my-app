b4e95f61e214aaed3ac8cfa3a0121d8c3927f644
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-06-10 09:00:05 +0200

    -testing currency filter

--- src/app/some-pipes/some-pipes.component.ts
+++ src/app/some-pipes/some-pipes.component.ts
@@ -4,8 +4,7 @@ import { TruncateWordsPipe } from './truncate-words.pipe';
 
 @Component({
   selector: 'my-some-pipes',
-  templateUrl: './some-pipes.component.html',
-  styleUrls: ['./some-pipes.component.scss']
+  templateUrl: './some-pipes.component.html'
 })
 export class SomePipesComponent implements OnInit {
 
@@ -17,6 +16,7 @@ export class SomePipesComponent implements OnInit {
   obj: Object = { foo: 'bar', baz: 'qux', nested: { xyz: 3, numbers: [1, 2, 3, 4, 5] } };
   vacation = new Date('2018-07-01T06:00:00+00:00');
   someDate = new Date(2019, 0, 10, 8, 0);
+  salary = 25000;
 
   ngOnInit() {
     this.truncateWords();
