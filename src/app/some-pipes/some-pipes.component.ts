de70affcf8f29c2d94b79ebe4ea8cbb1f7e13a75
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-06-23 20:38:44 +0200

    -testing phone numbers

--- src/app/some-pipes/some-pipes.component.ts
+++ src/app/some-pipes/some-pipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
+import { parseNumber, formatNumber } from 'libphonenumber-js';
 import { TruncateWordsPipe } from './truncate-words.pipe';
 
 @Component({
@@ -19,8 +20,9 @@ export class SomePipesComponent implements OnInit {
   salary = 25000;
 
   ngOnInit() {
-    this.truncateWords();
-    this.uppercaseWords();
+    // this.truncateWords();
+    // this.uppercaseWords();
+    this.formatPhones();
   }
 
   private truncateWords() {
@@ -33,6 +35,43 @@ export class SomePipesComponent implements OnInit {
     let words = 'Lorem ipsum';
     words = this.upperCasePipe.transform(words);
     console.log(words);
+  }
+
+  private formatPhones() {
+    const se = 'SE';
+    const international = 'International';
+
+    const sthlm01 = '08-123 45 678';
+    const sthlm01Parsed = parseNumber(sthlm01, se);
+
+    const sthlm02 = '0812345678';
+    const sthlm02Parsed = parseNumber(sthlm02, se);
+
+    const sthlm03 = '08 123 45 678';
+    const sthlm03Parsed = parseNumber(sthlm03, se);
+
+    const sthlm04 = '+46 8 123 456 78';
+    const sthlm04Parsed = parseNumber(sthlm04);
+
+    // It needs country code as parameter or as prefix on the phone string
+    // else it can not decide which country the phone belongs to
+
+    console.log(sthlm01Parsed, sthlm02Parsed, sthlm03Parsed, sthlm04Parsed);
+
+    const sthlm01Formated = formatNumber(sthlm01Parsed, international);
+    const sthlm02Formated = formatNumber(sthlm02Parsed, international);
+    const sthlm03Formated = formatNumber(sthlm03Parsed, international);
+    const sthlm04Formated = formatNumber(sthlm04Parsed, international);
+
+    // It seems SE is not valid as second argument. We use International.
+
+    console.log(`
+        ${sthlm01Formated}
+        ${sthlm02Formated}
+        ${sthlm03Formated}
+        ${sthlm04Formated}
+      `);
+
   }
 
 }
