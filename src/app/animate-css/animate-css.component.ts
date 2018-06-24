b0e91835926f567c6933fcc2a0e103a6547b90ec
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-06-24 12:32:45 +0200

    -trigger animation and reset when ends

--- src/app/animate-css/animate-css.component.ts
+++ src/app/animate-css/animate-css.component.ts
@@ -3,4 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     templateUrl: './animate-css.component.html',
 })
-export class AnimateCss {}
+export class AnimateCss {
+
+    triggerAnimation = false;
+
+    onTriggerAnimation() { 
+        this.triggerAnimation = true;
+        setTimeout(() => this.triggerAnimation = false, 1000);
+    }
+
+}
