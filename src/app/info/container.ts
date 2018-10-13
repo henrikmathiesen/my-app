6519c0b8905cdb4c65d82041af9a4cd724ae5254
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-10-13 10:13:02 +0200

    -restructure -ng-container doc

---
+++ src/app/info/container.ts
@@ -0,0 +1,15 @@
+// https://angular.io/guide/structural-directives#ng-container-to-the-rescue
+
+/*
+
+    <ng-container *ngIf="true">
+        <h2>Title</h2>
+        <div>Content</div>
+    </ng-container>
+
+    WILL PRODUCE
+
+    <h2>Title</h2>
+    <div>Content</div>
+
+*/
