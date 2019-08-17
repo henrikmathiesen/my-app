10f106ebf6febe6636ca24f9c10d794b1dad8256
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-08-17 05:54:09 +0200

    -comments

--- src/app/info/better-imports.ts
+++ src/app/info/better-imports.ts
@@ -1,6 +1,19 @@
 /* tslint:disable */
 
+// # Imports with index.ts file
+
+// index.ts file in folder 'models'
+// export * from './adress.model';
+// export * from './book.model';
+// export * from './reader.model';
+
+// some-other-file.ts
+// import { AdressModel, BookModel, ReaderModel } from './models';
+
+
+// # Import Shortcuts in tsconfig
+
 // https://netbasal.com/sexier-imports-in-typescript-e3c645bdd3c6
-// # See angular-x-routing
-// - tsconfig.json
-// - better-imports.component
+// - See angular-x-routing
+//      - tsconfig.json
+//      - better-imports.component
