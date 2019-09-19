14573962dbf0d0b53465e2a7be703cebbd5f3ade
Henrik Mathiesen <henrik.mathiesen@arbetsformedlingen.se>
2019-09-19 08:12:05 +0200

    -shift placeholder

---
+++ src/app/info/shift-placeholder.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+
+/*
+
+getPlaceHolder(): SomeModel {
+    const placeholder = new SomeModel();
+    placeholder.name = 'Select something';
+    placeholder.id = 0;
+
+    return placeholder;
+}
+
+shiftPlaceHolder(some: SomeModel[]): SomeModel[] {
+    const somr = [...some];
+    somr.unshift(this.getPlaceHolder());
+
+    return somr;
+}
+
+*/
