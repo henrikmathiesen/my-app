012809acae41ddae139d214c2096554ab27195d8
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-10-18 19:15:10 +0200

    -comments about renderer

---
+++ src/app/info/dom-manipulation.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+
+/*
+
+    https://alligator.io/angular/using-renderer2/
+    
+    https://angular.io/api/core/Renderer2
+
+    Prefer it over using vanilla DOM manipulation with ex querySelector or elementRef
+
+    
+    
+    Seems to be problems settings focus to an element though
+    https://github.com/angular/angular/issues/15674
+
+*/
