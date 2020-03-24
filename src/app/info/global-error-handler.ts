2b055ea3ccd8b1f458f212db1717ec617254fbe4
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-03-24 17:27:26 +0100

    -notes on error handling

--- src/app/info/global-error-handler.ts
+++ src/app/info/global-error-handler.ts
@@ -59,7 +59,7 @@ postSomething() {
 	.pipe(
         catchError((error) => {
             this.errorMessageService.setMessage('A specific error happened');		        // store in service, ready to communicate it to the user
-            throw error; 												                    // stops subscribe success callback from running, triggers handleError() in GlobalErrorHandler
+            throw new Error('reason'); 												        // stops subscribe success callback from running, triggers handleError() in GlobalErrorHandler
         })                                                                                  // return throwError(error); (from rxjs) does not seam to trigger GlobalErrorHandler
     );                                                                                      // complete callback in subscribe WILL run
 }                                                                                           // IF error callback in subscribe, need to throw error; from there else GlobalErrorHandler will not run
