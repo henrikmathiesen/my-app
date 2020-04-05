08a66beb1cdd00276306bc2a167350dc280e4a2b
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-04-05 09:18:07 +0200

    -comments

--- src/app/info/ng-template.ts
+++ src/app/info/ng-template.ts
@@ -3,4 +3,6 @@
     // https://blog.angular-university.io/angular-ng-template-ng-container-ngtemplateoutlet/
     // https://angular.io/guide/dynamic-component-loader
 
+    See also angular-x-rxjs6-plus > other > load-place-component for dynamic component placement
+
 */
