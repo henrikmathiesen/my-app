83cbf704a7506d609076ebc1f3c79888e71d84c0
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-04-20 16:49:28 +0200

    -info

--- src/app/info/base-href.ts
+++ src/app/info/base-href.ts
@@ -5,10 +5,12 @@
         - baseHref 
             - Will change  <base href="/my-app/"> in index.html
             - Will change background-image:url(/my-app/assets/cat.jpg);
+                - OBS Angular 8: https://github.com/angular/angular-cli/issues/14587
             - Will NOT change paths to css and js in index.html
             - https://shekhargulati.com/2017/07/06/angular-4-use-of-base-href-and-deploy-url-build-options/
         - deploy-url 
             - Will change paths to css and js in index.html
+            - Will change paths to lazy loaded module scripts
             - https://shekhargulati.com/2017/07/06/angular-4-use-of-base-href-and-deploy-url-build-options/
         - output-path
             - name of folder for output (default dist)
