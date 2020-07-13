c2ce6812d9423c6a9aa086fa61f44eac437a1f22
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-07-13 15:00:53 +0200

    -select compare with info

---
+++ src/app/info/select-compare-with.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+
+/* 
+
+    How to set selected option dynamically in Angular 6
+    https://medium.com/@kastepanyan24/how-to-set-selected-option-dynamically-in-angular-6-85c99958cca5
+
+*/
