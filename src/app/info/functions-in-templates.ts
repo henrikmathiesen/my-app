83a7bbceb96851ae3b5b7260e6c32da31afe4a57
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-07-13 10:18:03 +0200

    -notes on functions in templates

---
+++ src/app/info/functions-in-templates.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+
+/* 
+
+    Prefer not using functions in templates
+
+        https://medium.com/showpad-engineering/why-you-should-never-use-function-calls-in-angular-template-expressions-e1a50f9c0496
+
+    However Angular themselves use functions in templates
+
+        getVal()
+        https://angular.io/guide/template-syntax
+
+    And Todd Motto Does
+    
+        *ngIf="user.get('name').hasError('required') && user.get('name').touched"
+        https://ultimatecourses.com/blog/angular-2-forms-reactive
+
+*/
