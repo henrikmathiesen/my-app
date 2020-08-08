1363f6e79e9055624203c99a5eebd0f850e82139
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-08-08 12:34:11 +0200

    -comments

--- src/app/info/valid-date-string-from-user-input.ts
+++ src/app/info/valid-date-string-from-user-input.ts
@@ -10,6 +10,37 @@ export class ParsableDateChecker {
         // else value could be yyyyMM-dd or yyyy-MMdd for example
         // then this method would remove the dashes (in this scope), check Is reasonable date and say OK
 
+        /* 
+        
+            This should do it (from regular-expressions > basics-04.test.js)
+        
+            const validateDate = value => {
+                const pattern01 = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;      // yyyy-MM-dd
+                const pattern02 = /^\d{4}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;        // yyyyMMdd
+
+                const isValidPattern = pattern01.test(value) || pattern02.test(value);
+
+                if (isValidPattern === false) {
+                    return false;
+                }
+
+                // Is reasonable date
+
+                const noDashes = value.replace(/-/g, '');
+
+                const yearPart = noDashes.slice(0, 4);
+                const monthPart = noDashes.slice(4, 6);
+                const dayPart = noDashes.slice(6, 8);
+
+                const dateObj = new Date(`${yearPart}-${monthPart}-${dayPart}`);
+
+                return dateObj.getFullYear() === +yearPart &&
+                    (dateObj.getMonth() + 1) === +monthPart &&
+                    dateObj.getDate() === +dayPart;
+            };
+        
+        */
+
         const noDashes = value.replace(/-/g, '');
 
         const isNumbers = /^[0-9]*$/.test(noDashes);
