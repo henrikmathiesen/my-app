72dde4f885c7d88d9b85fd2d55b9d98ef1434a3f
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-08-21 18:23:28 +0200

    -info about setValue vs patchValue

---
+++ src/app/info/form-setvalue-vs-patchvalue.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+
+/*
+    
+    https://angular.io/guide/reactive-forms#updating-parts-of-the-data-model
+
+    Great explanations
+    https://medium.com/@samichkhachkhi/setvalue-vs-patchvalue-angular-a64a55e912b8
+    https://ultimatecourses.com/blog/angular-2-form-controls-patch-value-set-value
+
+*/
