c4082d0aa01487295df7abb39d1485399b5c8ce2
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-10-24 10:29:50 +0200

    -localization info

---
+++ src/app/info/localization-simple.ts
@@ -0,0 +1,30 @@
+/*
+
+// Works with ng serve and ng build --prod
+// No need for extra arguments to --prod
+// No need for entry in angular.json, just this is enough for general swedish culture
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeSe from '@angular/common/locales/sv';
+
+import { AppComponent } from './app.component';
+
+registerLocaleData(localeSe);
+
+@NgModule({
+  imports: [
+    BrowserModule,
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'sv-SE' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+*/
