ffc63ab35808a311897625960c84082609981cf6
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-10-31 17:17:38 +0100

    -comment on scss img url path and cli

--- src/app/info/base-href.ts
+++ src/app/info/base-href.ts
@@ -6,6 +6,11 @@
             - Will change  <base href="/my-app/"> in index.html
             - Will change background-image:url(/my-app/assets/cat.jpg);
                 - OBS Angular 8: https://github.com/angular/angular-cli/issues/14587
+                - OBS Fixed in Angular 10: https://github.com/angular/angular-cli/issues/18013
+                    - destus90 commented on , alan-agius4 commented on
+                        - "this is caused by a long outstanding issue which was solved in version 10"
+                    - Use releative from file: background-image: url('../../../assets/icon.svg');
+                    - Can put file in same folder as .scss file
             - Will NOT change paths to css and js in index.html
             - https://shekhargulati.com/2017/07/06/angular-4-use-of-base-href-and-deploy-url-build-options/
         - deploy-url 
