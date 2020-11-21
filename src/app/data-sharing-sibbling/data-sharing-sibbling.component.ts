7823980a56b3b6281e25229283fb63c76a69c60b
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-11-21 10:48:04 +0100

    -notes on NavigationExtras

--- src/app/data-sharing-sibbling/data-sharing-sibbling.component.ts
+++ src/app/data-sharing-sibbling/data-sharing-sibbling.component.ts
@@ -25,4 +25,10 @@ export class DataSharingSibblingComponent implements OnInit, OnDestroy {
     changeMessage() {
         this.dataSharingMediatorService.changeMessage('Hello from data-sharing-sibbling.component');
     }
+
+    /* 
+        NavigationExtras in Angular 7 plus, is an alternative
+        https://medium.com/coding-in-depth/best-use-of-navigationextras-in-angular-7-plus-d5fb436e298e
+    
+    */
 }
