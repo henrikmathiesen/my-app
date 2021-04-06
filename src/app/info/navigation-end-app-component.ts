fd801a7e963c06310f77ec7af62159866b82d9aa
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-04-06 17:18:21 +0200

    -notes on router subs

---
+++ src/app/info/navigation-end-app-component.ts
@@ -0,0 +1,31 @@
+/* tslint:disable */
+
+/*
+
+    import { Component, OnInit } from '@angular/core';
+    import { Router, NavigationEnd } from '@angular/router';
+    import { filter } from 'rxjs/operators';
+
+    @Component({
+        selector: 'app-root',
+        templateUrl: './app.component.html'
+    })
+    export class AppComponent implements OnInit {
+
+        constructor(
+            private router: Router
+        ) { }
+
+        ngOnInit() {
+            this.router.events
+                .pipe(
+                filter(event => event instanceof NavigationEnd)
+                )
+                .subscribe(() => {
+                    // do something in navigationEnd, call google analytics page view, for example
+                });
+        }
+
+    }
+
+*/
