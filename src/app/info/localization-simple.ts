c491913ffdd8dd8eb2720a2e90fb1f7bfaf2fd92
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2023-05-22 13:11:02 +0200

    Update localization-simple.ts

--- src/app/info/localization-simple.ts
+++ src/app/info/localization-simple.ts
@@ -3,7 +3,7 @@
 // Works with ng serve and ng build --prod
 // No need for extra arguments to --prod
 // No need for entry in angular.json, just this is enough for general swedish culture
-// projects.projectName.i18n.sourceLocale (: "sv-SE" or just "sv")
+// Entry angular.json: projects.projectName.i18n.sourceLocale (: "sv-SE" or just "sv")
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
